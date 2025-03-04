3cc371d6a5c81b24283e58dafaa500bcab803b06
PAPINO90 <papinimo2015@gmail.com>
2025-03-04 22:35:05 +0000

    Update expenses.controller.ts

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { ExpensesService } from './expenses.service';
 
 @Controller('expenses')
-export class ExpensesController {}
+export class ExpensesController {
+    constructor(private readonly expensesService: ExpensesService) {}
+
+    
+    @Get()
+    getAllExpenses() {
+        return this.expensesService.findAll();
+    }
+
+    @Post()
+    addExpense(@Body() expense: { title: string; amount: number }) {
+        return this.expensesService.create(expense);
+    }
+
+   
+    @Delete(':id')
+    deleteExpense(@Param('id') id: number) {
+        return this.expensesService.delete(id);
+    }
+}
