b9e838577a10c5f177fadd6f53caca42204b0d67
PAPINO90 <papinimo2015@gmail.com>
2025-03-04 22:39:00 +0000

    Update revenues.controller.ts

--- src/revenues/revenues.controller.ts
+++ src/revenues/revenues.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { RevenuesService } from './revenues.service';
 
 @Controller('revenues')
-export class RevenuesController {}
+export class RevenuesController {
+    constructor(private readonly revenuesService: RevenuesService) {}
+
+    @Get()
+    getAllRevenues() {
+        return this.revenuesService.findAll();
+    }
+
+   
+    @Post()
+    addRevenue(@Body() revenue: { title: string; amount: number }) {
+        return this.revenuesService.create(revenue);
+    }
+
+    @Delete(':id')
+    deleteRevenue(@Param('id') id: number) {
+        return this.revenuesService.delete(id);
+    }
+}
