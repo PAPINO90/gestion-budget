85f981e636b417148d8bc5591a760fef21e6f62c
PAPINO90 <papinimo2015@gmail.com>
2025-03-04 22:37:00 +0000

    Update expenses.service.ts

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExpensesService {}
+export class ExpensesService {
+    private expenses = [
+        { id: 1, title: 'Nourriture', amount: 15000 },
+        { id: 2, title: 'Transport', amount: 5000 }
+    ];
+
+    
+    findAll() {
+        return this.expenses;
+    }
+
+  
+    create(expense: { title: string; amount: number }) {
+        const newExpense = { id: Date.now(), ...expense };
+        this.expenses.push(newExpense);
+        return newExpense;
+    }
+
+   
+    delete(id: number) {
+        this.expenses = this.expenses.filter(exp => exp.id !== id);
+        return { message: `Dépense avec l'ID ${id} supprimée` };
+    }
+}
